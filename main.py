ac29e32d107581a504635448ff986e13cc7fb367
Nurservis <147647011+Nurservis@users.noreply.github.com>
2023-10-15 15:02:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from fastapi import FastAPI, UploadFile, File, status
 from shazamio import Shazam
 import tempfile
 import random
-from fastapi import HTTPException
+
+from fastapi import FastAPI, HTTPException, status
+
 from fastapi.encoders import jsonable_encoder
-from bs4 import BeautifulSoup as BS
 import requests
+import random
+
+
+import logging
+
+from bs4 import BeautifulSoup as BS
 
 def random_ip():
     ips = ['46.227.123.', '37.110.212.', '46.255.69.', '62.209.128.', '37.110.214.', '31.135.209.', '37.110.213.']
@@ -23,23 +30,25 @@ async def recognize_url(url: str):
     if not url:
         logging.error(f"{datetime.datetime.now()} - No URL provided")
         return {"status": False, "message": "No URL provided"}
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                if resp.status != 200:
-                    logging.error(f"{datetime.datetime now()} - {resp.status}")
-                    result = {"status": False, "message": "Invalid URL"}
-                else:
-                    logging.info("Recognizing song from audio URL.")
-                    bytes_file = await resp.read()
-                    if len(bytes_file) > 20971520:
-                        result = {"status": False, "message": "File size is too large"}
+    else:
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    if resp.status != 200:
+                        logging.error(f"{datetime.datetime.now()} - {resp.status}")
+                        result = {"status": False, "message": "Invalid URL"}
                     else:
-                        result = await shazam.recognize_song(bytes_file)
-        return result
-    except Exception as e:
-        logging.error(f"{datetime.datetime.now()} - {e}")
-        return {"status": False, "message": "Something went wrong"}
+                        logging.info("Recognizing song from audio URL.")
+                        bytes_file = await resp.read()
+                        if len(bytes_file) > 20971520:
+                            result = {"status": False, "message": "File size is too large"}
+                        else:
+                            result = await shazam.recognize_song(bytes_file)
+                        await session.close()
+                    return result
+        except Exception as e:
+            logging.error(f"{datetime.datetime.now()} - {e}")
+            return {"status": False, "message": "Something went wrong"}
 
 @app.post("/recognize-song")
 async def recognize_song(file: UploadFile):
@@ -49,9 +58,10 @@ async def recognize_song(file: UploadFile):
             audio_path = temp_audio.name
             shazam = Shazam()
             result = await shazam.recognize_song(audio_path)
-        return result
+            return result
     except Exception as e:
         return {"error": str(e)}
+
 
 @app.get("/instagram/", response_model=dict)
 async def instagram(url: str):
@@ -70,6 +80,7 @@ async def instagram(url: str):
     response = requests.post(base_url, data=data, headers=headers)
     jsonn = response.json()
     try:
+
         if jsonn['status'] == 'ok':
             data = jsonn['data']
             soup = BS(data, 'html.parser')
@@ -87,6 +98,7 @@ async def instagram(url: str):
     except Exception as err:
         return {"error": "This video is private"}
 
+
 @app.post("/pinterest/")
 async def get_pinterest_info(link: str):
     try:
@@ -100,8 +112,9 @@ async def get_pinterest_info(link: str):
         d = {
             'url': link
         }
+
         r = requests.post(url, headers=header, data=d)
-        r.raise_for_status()
+        r.raise_for_status()  # Raise exception for HTTP errors
         soup = BS(r.text, 'html.parser')
         b = soup.select_one("#quality_1 > option:nth-child(2)")
         a = soup.find("div", class_="download-items")
@@ -110,15 +123,18 @@ async def get_pinterest_info(link: str):
             i = a.a.get("href")
         else:
             raise HTTPException(status_code=404, detail="Data not found on the page")
+
         if b is not None:
             result = b["value"]
         else:
             result = i
+
         return {"result": result}
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code=500, detail="Error in the external request")
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal server error")
+
 
 async def get_mp3_url_yt(youtube_url):
     api_url = 'https://yt5s.io/api/ajaxSearch'
@@ -126,6 +142,7 @@ async def get_mp3_url_yt(youtube_url):
         'q': youtube_url,
         'vt': 'mp3'
     }
+
     async with aiohttp.ClientSession() as session:
         async with session.post(api_url, data=data, headers={'X-Requested-With': 'XMLHttpRequest'}) as response:
             if response.status == 200:
@@ -134,12 +151,14 @@ async def get_mp3_url_yt(youtube_url):
             else:
                 print(f"Xatolik yuz berdi: {response.status}")
 
+
 async def get_all_mp3_urls(data):
     base_url = 'https://ve44.aadika.xyz/download/'
     video_id = data['vid']
     time_expires = data['timeExpires']
     token = data['token']
     mp3_info_dict = {}
+
     for index, (quality, info) in enumerate(data['links']['mp3'].items(), start=1):
         if info['f'] == 'mp3':
             mp3_info = {
@@ -151,20 +170,25 @@ async def get_all_mp3_urls(data):
                 'url': f"{base_url}{video_id}/mp3/{info['k']}/{time_expires}/{token}/{index}?f=yt5s.io"
             }
             mp3_info_dict[index] = mp3_info
+
     return mp3_info_dict
 
-@app.get("/youtube/download/audio/", status_code=status.HTTP_200_OK, description="Download audio from Youtube", tags=['youtube'])
+
+@app.get("/youtube/download/audio/", status_code=status.HTTP_200_OK, description="Download audio from Youtube",
+         tags=['youtube'])
 async def youtube_audio_url(url: str):
     if not url:
         logging.error(f"{datetime.datetime.now()} - No url provided")
         return {"status": False, "message": "No url provided"}
-    try:
-        result = await get_mp3_url_yt(url)
-        if result['mess'] == "":
-            res = await get_all_mp3_urls(result)
-            return {"status": True, "result": res}
-        else:
+    else:
+        try:
+            result = await get_mp3_url_yt(url)
+            if result['mess'] == "":
+                res = await get_all_mp3_urls(result)
+                return {"status": True, "result": res}
+            else:
+                return {"status": False, "message": "Something went wrong"}
+
+        except Exception as e:
+            logging.error(f"{datetime.datetime.now()} - {e}")
             return {"status": False, "message": "Something went wrong"}
-    except Exception as e:
-        logging.error(f"{datetime.datetime.now()} - {e}")
-        return {"status": False, "message": "Something went wrong"}
